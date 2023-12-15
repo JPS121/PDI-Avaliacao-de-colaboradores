27b7f4b5fa443da55a413c4653391ccbcf4e9166
João Pedro <jvieirasousa121@hotmail.com>
2023-12-15 16:19:59 -0300

    first commit

---
+++ api/src/test/java/jsp/pdi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp.pdi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
